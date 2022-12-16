550071c2fadb8bd1764f99d214f84287c05b3db1
Casey Dwyer <caseyjdwyer@gmail.com>
2022-12-15 22:48:15 -0800

    Set framework to allow for setting and getting local storage

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,17 @@
+// Current day for initial page load and current weather conditions
 let now = dayjs().format("M/D/YYYY");
 console.log(now);
 
-// Search for City event listener to get user value
+// City list local storage
+let cityList = [];
+
+// CLICK - Search for City event listener to get user value
 $("#searchForm").submit(function () {
   event.preventDefault();
   const userInput = $("#searchFormInput").val();
   console.log(userInput);
   getWeather(userInput);
+  storeCity(userInput);
   clearForm();
 });
 
@@ -23,7 +28,6 @@ function getWeather(searchInput) {
     .then((response) => response.json())
     .then(function (data) {
       // lat and lon for Current Weather API
-      // TO DO - You can make this into one geoCoding Obj with each thing as a property
       const lat = data[0].lat;
       const lon = data[0].lon;
 
@@ -78,42 +82,6 @@ function getWeather(searchInput) {
     });
 }
 
-// Five Day Forecast function
-function fiveDay(data) {
-  console.log(data);
-  // 5 day forecast hard coded into index to pick 5 values from forecast data
-  const fiveDayIndex = 5;
-  for (i = 0; i < fiveDayIndex; i++) {
-    // The Openweather 5 Day forecast gives 5 days of forecast data in 3 hour increments.
-    // Choosing afternoon time to display most user-relevant weather conditions
-    const afternoonIndex = i * 8 + 2;
-
-    // variables to put into cards
-    const fiveDate = data.list[afternoonIndex].dt_txt;
-    const fiveIconCode = data.list[afternoonIndex].weather[0].icon;
-    const fiveTemp = data.list[afternoonIndex].main.temp;
-    const fiveWind = data.list[afternoonIndex].wind.speed;
-    const fiveHumid = data.list[afternoonIndex].main.humidity;
-    const fiveIconURL =
-      "http://openweathermap.org/img/wn/" + fiveIconCode + ".png";
-
-    console.log(data.list[afternoonIndex]);
-
-    // create elements
-    const newCard = `<div class="card border-info mb-3" style="max-width: 18rem">
-    <div class="card-body">
-      <h5 class="card-title">${fiveDate}</h5>
-      <img src="${fiveIconURL}" alt="Weather icon">
-      <p class="card-text">Temp: ${fiveTemp} °F</p>
-      <p class="card-text">Wind: ${fiveWind}mph</p>
-      <p class="card-text">Humidity: ${fiveHumid}%</p>
-    </div>
-  </div>`;
-    // append to html
-    $("#five-cont").append(newCard);
-  }
-}
-
 // Current weather conditions function
 function currentWeather(data) {
   // Variables to set text
@@ -134,8 +102,66 @@ function currentWeather(data) {
   $("#currentHumid").text(currentHumid + "%");
 }
 
+// Five Day Forecast function
+function fiveDay(data) {
+  // reset html
+  $("#five-cont").html("");
+
+  console.log(data);
+  // 5 day forecast hard coded into index to pick 5 values from forecast data
+  const fiveDayIndex = 5;
+  for (i = 0; i < fiveDayIndex; i++) {
+    // The Openweather 5 Day forecast gives 5 days of forecast data in 3 hour increments...
+    // Choosing afternoon time to display most user-relevant weather conditions
+    const afternoonIndex = i * 8 + 2;
+
+    // variables to put into cards
+    const fiveDate = data.list[afternoonIndex].dt_txt;
+    const fiveIconCode = data.list[afternoonIndex].weather[0].icon;
+    const fiveTemp = data.list[afternoonIndex].main.temp;
+    const fiveWind = data.list[afternoonIndex].wind.speed;
+    const fiveHumid = data.list[afternoonIndex].main.humidity;
+    const fiveIconURL =
+      "http://openweathermap.org/img/wn/" + fiveIconCode + ".png";
+
+    console.log(data.list[afternoonIndex]);
+
+    // create elements
+    const newCard = `<div class="card border-info mb-3" style="max-width: 18rem">
+      <div class="card-body">
+        <h5 class="card-title">${fiveDate}</h5>
+        <img src="${fiveIconURL}" alt="Weather icon">
+        <p class="card-text">Temp: ${fiveTemp} °F</p>
+        <p class="card-text">Wind: ${fiveWind}mph</p>
+        <p class="card-text">Humidity: ${fiveHumid}%</p>
+      </div>
+    </div>`;
+    // append to html
+    $("#five-cont").append(newCard);
+  }
+}
+
 // Function to clear any form text after entry
 function clearForm() {
   // Search for a city input
   $("#searchFormInput").val("");
 }
+
+// Local storage setItem
+
+function storeCity(userInput) {
+  let cityList = cityList.unshift(userInput);
+ JSON.stringify(localStorage.setItem("city", cityList));
+  displayCity();
+}
+
+// Local storage getItem and display
+
+function displayCity() {
+  let cityList = JSON.parse(localStorage.getItem("city"));
+  console.log(cityList);
+  // for each create a button in the searched items list
+}
+
+// Initialize displayCity on page load
+displayCity();
