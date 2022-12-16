c705ee6d6ef6dc726cad0da3c15c1322b134fa89
Casey Dwyer <caseyjdwyer@gmail.com>
2022-12-15 16:50:17 -0800

    Get city information from Geocoding API

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,45 @@
-// Search for City event listener
-const cityValue = $("#searchForm").submit(function () {
+// Search for City event listener to get user value
+$("#searchForm").submit(function () {
   event.preventDefault();
   const userInput = $("#searchFormInput").val();
   console.log(userInput);
-  return userInput;
+  geoCoding(userInput);
+  clearForm();
 });
+
+// Geocoding to get lat/long from city name
+function geoCoding(searchInput) {
+  const geoCodingURL =
+    "http://api.openweathermap.org/geo/1.0/direct?q=" +
+    searchInput +
+    "&limit=1&appid=1818fcd056aa18519062741f9933b26b";
+
+  fetch(geoCodingURL)
+    .then((response) => response.json())
+    .then(function (data) {
+      // lat and lon for Current Weather API
+      const lat = data[0].lat;
+      const lon = data[0].lon;
+      const cityName = data[0].name;
+      const cityState = data[0].state;
+      const cityCountry = data[0].country;
+      console.log(
+        "City: " +
+          cityName +
+          " State: " +
+          cityState +
+          " Country: " +
+          cityCountry +
+          " lat/lon: " +
+          lat +
+          "/" +
+          lon
+      );
+    });
+}
+
+// Function to clear any form text after entry
+function clearForm() {
+  // Search for a city input
+  $("#searchFormInput").val("");
+}
